2614ab9f54c1771657f5d97ddf0cf2d34a585038
meyirman-is-creator <sarsenbaymeyirman@gmail.com>
2024-02-11 21:45:10 +0600

    i updated

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let content = document.getElementById('content');
-
+let sportBtn = document.getElementById('sport');
+let businessBtn = document.getElementById('business');
+let technologyBtn = document.getElementById('technology');
+let musicBtn = document.getElementById('music');
 function formatDateString(dateString) {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' };
     const date = new Date(dateString);
@@ -23,13 +26,13 @@ function getContentItem(data) {
                     <img src=${data.urlToImage!==null ? data.urlToImage : "img/bg.webp"} alt="">
                 </div>`
 }
-document.addEventListener('DOMContentLoaded', (event) => {
-
-    fetch('https://newsapi.org/v2/top-headlines?country=us&category=sport&apiKey=20e4c41c4d904ff49f10485c8aafa3b2')
-        .then((response) => response.json())
-        .then(data => {
-             
-            data.articles.forEach(element => {
+function switchCategory(category){
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=20e4c41c4d904ff49f10485c8aafa3b2`)
+    .then((response) => response.json())
+    .then(data => {
+        content.innerHTML="";
+        data.articles.forEach(element => {
+            if('[Removed]'!==element.title){
                 let div = document.createElement('div');
                 div.classList.add('content__item');
                 div.innerHTML+=getContentItem(element);
@@ -38,7 +41,52 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     localStorage.setItem('active', JSON.stringify(element));
                     window.open('about.html', '_self');
                 })
-            });
+            }
+            
         });
+    });
+};
+document.addEventListener('DOMContentLoaded', (event) => {
+    switchCategory('sport')
+});
+
+sportBtn.addEventListener('click', ()=>{
+    if(!sportBtn.classList.contains('category__button-active')){
+        switchCategory('sport');
+        sportBtn.classList.add('category__button-active');
+        businessBtn.classList.remove('category__button-active');
+        musicBtn.classList.remove('category__button-active');
+        technologyBtn.classList.remove('category__button-active');
+    }
+
+});
+businessBtn.addEventListener('click', ()=>{
+    if(!businessBtn.classList.contains('category__button-active')){
+        switchCategory('business');
+        sportBtn.classList.remove('category__button-active');
+        businessBtn.classList.add('category__button-active');
+        musicBtn.classList.remove('category__button-active');
+        technologyBtn.classList.remove('category__button-active');
+    }
+
+});
+musicBtn.addEventListener('click', ()=>{
+    if(!musicBtn.classList.contains('category__button-active')){
+        switchCategory('music');
+        sportBtn.classList.remove('category__button-active');
+        businessBtn.classList.remove('category__button-active');
+        musicBtn.classList.add('category__button-active');
+        technologyBtn.classList.remove('category__button-active');
+    }
+
+});
+technologyBtn.addEventListener('click', ()=>{
+    if(!technologyBtn.classList.contains('category__button-active')){
+        switchCategory('technology');
+        sportBtn.classList.remove('category__button-active');
+        businessBtn.classList.remove('category__button-active');
+        musicBtn.classList.remove('category__button-active');
+        technologyBtn.classList.add('category__button-active');
+    }
 
 });
