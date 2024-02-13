5797ecc6fe29b9d1ddc9f365b6c27a8df73a36b7
meyirman-is-creator <sarsenbaymeyirman@gmail.com>
2024-02-13 15:31:40 +0600

    i added favorite function

--- about.js
+++ about.js
@@ -1,5 +1,7 @@
 let backBtn = document.getElementById('back');
 let articleContent = document.getElementById('articleContent');
+let articleContentBtn = document.getElementById('articleContentBtn');
+let favorites =[];
 function formatDateString(dateString) {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' };
     const date = new Date(dateString);
@@ -14,6 +16,7 @@ function getContentItem(data) {
                         <div class="upload__inner-date">${formatDateString(data.publishedAt)}</div>
                     </div>
                 </div>
+                
             </div>
             <div class="article__title">${data.title}</div>
             <div class="article__description">${data.description}</div>
@@ -21,12 +24,40 @@ function getContentItem(data) {
             <div class="article__name">${data.source.name}</div>
             <div class="article__text">${data.content}</div>`
 }
+let element;
 document.addEventListener('DOMContentLoaded', (event) => {
-    const element = JSON.parse(localStorage.getItem('active'));
-    articleContent.innerHTML = getContentItem(element);
+    favorites = JSON.parse(localStorage.getItem('favorites'));
+
+    element = JSON.parse(localStorage.getItem('active'));
+    if(favorites!==null && favorites.length>0){
+        for(let i =0;i<favorites.length; i++){
+            if(element.title===favorites[i].title){
+                articleContentBtn.classList.add('article__btn--active');
+                
+            }
+        }
+    }
+    
+    articleContent.innerHTML += getContentItem(element);
 });
+
 backBtn.addEventListener('click', ()=>{
     localStorage.setItem('active', '');
 
     window.open('index.html', '_self');
 });
+articleContentBtn.addEventListener('click',()=>{
+    if(articleContentBtn.classList.contains('article__btn--active')){
+        articleContentBtn.classList.remove('article__btn--active');
+        for(let i=0;i<favorites.length;i++){
+            if(favorites[i].title===element.title){
+                favorites =favorites.filter(item=>item.title!==favorites[i].title);
+                localStorage.setItem('favorites', JSON.stringify(favorites));
+            }
+        }
+    } else{
+        articleContentBtn.classList.add('article__btn--active');
+        favorites.push(element);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+})
--- index.js
+++ index.js
@@ -3,90 +3,194 @@ let sportBtn = document.getElementById('sport');
 let businessBtn = document.getElementById('business');
 let technologyBtn = document.getElementById('technology');
 let musicBtn = document.getElementById('music');
+let favorite = document.getElementById('favorite');
+let toFavorite = document.getElementById('toFavorite');
+let favorites = [];
 function formatDateString(dateString) {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' };
     const date = new Date(dateString);
     return date.toLocaleDateString('en-US', options);
 }
-function getContentItem(data) {
+function getContentItem(data, category) {
     return `
                 <div class="content__item-info">
                     <div class="content__item-author">
                         <div class="author__photo"><img src="img/user.jpg" alt=""></div>
                         <div class="author__name">${data.author === null ? 'Authors name' : data.author}<span> in</span> Topics Name</div>
-                        <div class="data__content">${data.publishedAt===null ? '': formatDateString(data.publishedAt)}</div>
+                        <div class="data__content">${data.publishedAt === null ? '' : formatDateString(data.publishedAt)}</div>
                     </div>
                     <div class="content__item-title">${data.title}</div>
-                    <div class="content__item-describtion">${data.description===null ? '': data.description}</div>
+                    <div class="content__item-describtion">${data.description === null ? '' : data.description}</div>
                     <div class="info__content">
-                        <button type="button" class="teg">JavaScript</button>
+                        <button type="button" class="teg">${category}</button>
                     </div>
                 </div>
                 <div class="content__item-img">
-                    <img src=${data.urlToImage!==null ? data.urlToImage : "img/bg.webp"} alt="">
+                    <img src=${data.urlToImage !== null ? data.urlToImage : "img/bg.webp"} alt="">
                 </div>`
 }
-function switchCategory(category){
-    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=20e4c41c4d904ff49f10485c8aafa3b2`)
-    .then((response) => response.json())
-    .then(data => {
-        content.innerHTML="";
-        data.articles.forEach(element => {
-            if('[Removed]'!==element.title){
+async function switchCategory(category) {
+    await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=20e4c41c4d904ff49f10485c8aafa3b2`)
+        .then((response) => response.json())
+        .then(data => {
+            content.innerHTML = "";
+            data.articles.forEach(element => {
+                if ('[Removed]' !== element.title) {
+                    let div = document.createElement('div');
+                    div.classList.add('content__item');
+                    div.innerHTML += getContentItem(element, category);
+                    let fav = document.createElement('button');
+                    fav.classList.add('favorite');
+                    fav.innerHTML += '<i class="fi fi-rs-heart"></i>';
+
+                    if (favorites !== null) {
+                        for (let i = 0; i < favorites.length; i++) {
+                            if (favorites[i].title === element.title) {
+                                fav.innerHTML = '';
+                                fav.classList.add('favorite__active');
+                                fav.innerHTML += '<i class="fi fi-ss-heart"></i>';
+                            }
+                        }
+                    }
+
+                    fav.addEventListener('click', (event) => {
+                        event.stopPropagation();
+                        if (fav.classList.contains('favorite__active')) {
+                            for (let i = 0; i < favorites.length; i++) {
+                                if (favorites[i].title === element.title) {
+                                    favorites = favorites.filter(item => item.title !== favorites[i].title);
+                                }
+                            }
+                            localStorage.setItem('favorites', JSON.stringify(favorites));
+                            fav.classList.remove('favorite__active');
+                            fav.innerHTML = '';
+                            fav.innerHTML = '<i class="fi fi-rs-heart"></i>';
+                        } else {
+
+                            if (favorites !== null) favorites.push(element);
+                            else favorites = [element];
+                            localStorage.setItem('favorites', JSON.stringify(favorites));
+
+                            fav.classList.add('favorite__active');
+
+                            fav.innerHTML = '<i class="fi fi-ss-heart"></i>';
+                        }
+
+                    });
+                    div.appendChild(fav);
+                    content.appendChild(div);
+
+                    div.addEventListener('click', () => {
+                        localStorage.setItem('active', JSON.stringify(element));
+                        window.open('about.html', '_self');
+                    })
+                }
+
+            });
+        });
+};
+document.addEventListener('DOMContentLoaded', (event) => {
+    favorites = JSON.parse(localStorage.getItem('favorites'));
+    switchCategory('sport')
+});
+toFavorite.addEventListener('click', () => {
+    if (!toFavorite.classList.contains('category__button-active')) {
+        content.innerHTML = "";
+        sportBtn.classList.remove('category__button-active');
+        businessBtn.classList.remove('category__button-active');
+        musicBtn.classList.remove('category__button-active');
+        technologyBtn.classList.remove('category__button-active');
+        toFavorite.classList.add('category__button-active');
+        if (favorites !== null && favorites.length > 0) {
+            favorites.forEach(element=>{
                 let div = document.createElement('div');
                 div.classList.add('content__item');
-                div.innerHTML+=getContentItem(element);
+                div.innerHTML += getContentItem(element);
+                let fav = document.createElement('button');
+                fav.classList.add('favorite');
+                fav.classList.add('favorite__active');
+                fav.innerHTML = '<i class="fi fi-ss-heart"></i>';
+                fav.addEventListener('click', (event) => {
+                    event.stopPropagation();
+                    if (fav.classList.contains('favorite__active')) {
+                        const index = favorites.findIndex(obj => obj.title === element.title);
+
+                        if (index !== -1) {
+                            favorites.splice(index, 1);
+                        }
+                        localStorage.setItem('favorites', JSON.stringify(favorites));
+                        fav.classList.remove('favorite__active');
+                        fav.innerHTML = '';
+                        fav.innerHTML = '<i class="fi fi-rs-heart"></i>';
+                    } else {
+
+                        if (favorites !== null) {
+                            favorites.push(element);
+                        }
+                        else favorites = [element];
+                        localStorage.setItem('favorites', JSON.stringify(favorites));
+                     
+
+                        fav.classList.add('favorite__active');
+
+                        fav.innerHTML = '<i class="fi fi-ss-heart"></i>';
+                    }
+                });
+
+                div.appendChild(fav);
                 content.appendChild(div);
                 div.addEventListener('click', () => {
                     localStorage.setItem('active', JSON.stringify(element));
                     window.open('about.html', '_self');
-                })
-            }
-            
-        });
-    });
-};
-document.addEventListener('DOMContentLoaded', (event) => {
-    switchCategory('sport')
-});
+                })  
+            });
 
-sportBtn.addEventListener('click', ()=>{
-    if(!sportBtn.classList.contains('category__button-active')){
+        } else {
+            content.innerHTML = "Favorite is empty";
+        }
+    }
+});
+sportBtn.addEventListener('click', () => {
+    if (!sportBtn.classList.contains('category__button-active')) {
         switchCategory('sport');
         sportBtn.classList.add('category__button-active');
         businessBtn.classList.remove('category__button-active');
         musicBtn.classList.remove('category__button-active');
         technologyBtn.classList.remove('category__button-active');
+        toFavorite.classList.remove('category__button-active');
     }
 
 });
-businessBtn.addEventListener('click', ()=>{
-    if(!businessBtn.classList.contains('category__button-active')){
+businessBtn.addEventListener('click', () => {
+    if (!businessBtn.classList.contains('category__button-active')) {
         switchCategory('business');
         sportBtn.classList.remove('category__button-active');
         businessBtn.classList.add('category__button-active');
         musicBtn.classList.remove('category__button-active');
         technologyBtn.classList.remove('category__button-active');
+        toFavorite.classList.remove('category__button-active');
     }
 
 });
-musicBtn.addEventListener('click', ()=>{
-    if(!musicBtn.classList.contains('category__button-active')){
+musicBtn.addEventListener('click', () => {
+    if (!musicBtn.classList.contains('category__button-active')) {
         switchCategory('music');
         sportBtn.classList.remove('category__button-active');
         businessBtn.classList.remove('category__button-active');
         musicBtn.classList.add('category__button-active');
         technologyBtn.classList.remove('category__button-active');
+        toFavorite.classList.remove('category__button-active');
     }
 
 });
-technologyBtn.addEventListener('click', ()=>{
-    if(!technologyBtn.classList.contains('category__button-active')){
+technologyBtn.addEventListener('click', () => {
+    if (!technologyBtn.classList.contains('category__button-active')) {
         switchCategory('technology');
         sportBtn.classList.remove('category__button-active');
         businessBtn.classList.remove('category__button-active');
         musicBtn.classList.remove('category__button-active');
         technologyBtn.classList.add('category__button-active');
+        toFavorite.classList.remove('category__button-active');
     }
 
 });
